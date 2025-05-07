1718170d60bf5c86956a67efef62a4b27d70051b
jason <jason@zgwit.com>
2025-05-07 11:44:07 +0800

    导入能用了

--- src/app/template/import/import.component.ts
+++ src/app/template/import/import.component.ts
@@ -1,9 +1,9 @@
 import {Component, inject} from '@angular/core';
 import {NzUploadComponent, NzUploadXHRArgs} from 'ng-zorro-antd/upload';
-import {read, utils} from 'xlsx';
+import {read, utils, writeFile} from 'xlsx';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
 import {Subscription} from 'rxjs';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {
   NzTableCellDirective,
   NzTableComponent,
@@ -20,6 +20,11 @@ import {isFunction} from 'rxjs/internal/util/isFunction';
 import {LinkReplaceParams} from '../../lib/utils';
 import {NzSelectComponent} from 'ng-zorro-antd/select';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {NzStepComponent, NzStepsComponent} from 'ng-zorro-antd/steps';
+import {NzProgressComponent} from 'ng-zorro-antd/progress';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {NzResultComponent} from 'ng-zorro-antd/result';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-import',
@@ -37,6 +42,11 @@ import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular
     NzSpinComponent,
     NzSelectComponent,
     ReactiveFormsModule,
+    NzStepsComponent,
+    NzStepComponent,
+    NzProgressComponent,
+    NzResultComponent,
+    NgIf,
   ],
   templateUrl: './import.component.html',
   standalone: true,
@@ -45,20 +55,30 @@ import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular
 export class ImportComponent extends TemplateBase {
   datum: any = []
 
+  values: any[] = []
+
+  succeed: any[] = []
+  failed: any[] = []
+
+  percent = 0
+
+  current: number = 0;
+
   group: FormGroup = new FormGroup([])
   options: any = [{value: -1, label: '-'}]
 
   fb = inject(FormBuilder)
-  headers: any = {}
+  ns = inject(NzNotificationService)
+  //headers: any = {}
 
-  onMount() {
-    console.log("[import]", "onMount")
+  // onMount() {
+  //   console.log("[import]", "onMount")
+  //
+  //   //this.buildForm()
+  // }
 
-    const content = this.content as ImportContent
-    if (!content) return
+  report = ""
 
-    this.buildForm()
-  }
 
   onUploadRequest = (args: NzUploadXHRArgs): Subscription => {
     console.log("onUploadRequest", args);
@@ -83,8 +103,13 @@ export class ImportComponent extends TemplateBase {
       let aoa = utils.sheet_to_json(sheet, {header: 1})
       this.datum = aoa
 
-      //this.buildForm()
+
+      this.buildForm()
       this.findValues()
+      this.current = 1
+
+      //this.buildForm()
+      //this.findValues()
     }
     //@ts-ignore
     reader.readAsArrayBuffer(args.file)
@@ -100,36 +125,102 @@ export class ImportComponent extends TemplateBase {
       group[c.key] = new FormControl(-1) //[-1];
     })
     this.group = this.fb.group(group)
-
   }
 
   findValues() {
+    const content = this.content as ImportContent
+    if (!content) return
+
     let row: any = this.datum[0] || []
+
+    console.log("row", row)
+    let headers: any = {}
 
     this.options = [{value: -1, label: '-'}]
     row.forEach((h: string, i: number) => {
       let ss = h.split("/")
-      let key = (ss.length > 1) ? ss[1] : ss[0]
-
-      //识别列名
-      let c = this.group.get(key)
-      if (c) {
-        c.setValue(i)
-      }
-      //this.headers[key] = i
+      ss.forEach((v: string) => {
+        let key = v.trim()
+        content.columns.forEach(c => {
+          if (c.key == key || c.label == key)
+            headers[c.key] = i
+        })
+      })
 
       this.options.push({value: i, label: h,})
     })
+
+    //console.log("options", this.options)
+    console.log("headers", headers)
+    // setTimeout(()=>{
+    //   this.group.patchValue(headers)
+    // }, 100)
+
+    this.group.patchValue(headers)
   }
 
-  submitting = false;
-
-  submit() {
-    if (this.submitting) return
-    console.log("[import] submit", this.page)
+  downloadTemplate() {
     const content = this.content as ImportContent
     if (!content) return
 
+    const aoa = content.columns.map(c => {
+      if (c.label) return c.label + '/' + c.key
+      else return c.key
+    })
+    const sheet = utils.aoa_to_sheet([aoa])
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, sheet)
+
+    // if(!wb.Props) wb.Props = {};
+    // wb.Props.Title = "Template";
+    // wb.Props.Author = "Boat";
+
+    //const filename = (this.app||'') + (this.page||'') + "-template.xlsx"
+    const filename = [this.app, this.page, "template.xlsx"].filter(i => i).join("-")
+    writeFile(wb, filename, {compression: true});
+  }
+
+  back() {
+    this.current--
+  }
+
+  preview() {
+    this.current = 2
+
+    this.values = []
+
+    let fields = this.group.value
+    for (let i = 1; i < this.datum.length; i++) {
+      let obj: any = {}
+      let row: any[] = this.datum[i]
+      for (const k in fields) {
+        const index = fields[k]
+        if (index > -1 && index < row.length) {
+          obj[k] = row[index]
+          //todo 处理日期格式
+        }
+      }
+      this.values.push(obj)
+    }
+  }
+
+  removeValue(i: number): void {
+    this.values.splice(i, 1)
+  }
+
+
+  uploading = false;
+
+  async upload() {
+    this.current = 3
+
+    if (this.uploading) return
+    console.log("[import] submit", this.page)
+
+    const content = this.content as ImportContent
+    if (!content) return
+
+    //编译函数
     if (typeof content.submit == "string" && content.submit.length > 0) {
       try {
         content.submit = new Function("data", content.submit)
@@ -137,30 +228,72 @@ export class ImportComponent extends TemplateBase {
         console.error(e)
       }
     }
-    if (isFunction(content.submit)) {
-      this.submitting = true
-      content.submit.call(this, this.data).then((res: any) => {
-        //this.data = res;
-        //this.ns.success("提示", "提交成功")
-      }).finally(() => {
-        this.submitting = false
-      })
-    } else if (content.submit_api) {
-      this.submitting = true
-      let url = LinkReplaceParams(content.submit_api, this.params);
 
+    //逐一上传
+    for (let i = 0; i < this.values.length; i++) {
+      let row = this.values[i]
+      try {
+        let obj = await this.uploadRow(row)
 
-      //TODO 这里开始上传
-      this.request.post(url, {}).subscribe(res => {
-        if (res.error) return
-        this.data = res.data
-        //this.ns.success("提示", "提交成功")
+        this.succeed.push(obj)
+      } catch (e: any) {
+        row["_ERROR_"] = e
+        this.failed.push(row)
+        //this.ns.error("错误", e)
+      }
 
-      }).add(() => {
-        this.submitting = false
-      })
+      this.percent = Math.floor((this.succeed.length + this.failed.length) * 100 / this.values.length)
     }
+
+    //完成
+    this.current = 4
+
+    this.report = `总计${this.values.length}条，已经上传${this.succeed.length}条，失败${this.failed.length}条`
   }
 
+  async uploadRow(row: any) {
+    const content = this.content as ImportContent
+    if (!content) return
 
+    if (isFunction(content.submit)) {
+      return content.submit.call(this, this.data)
+    }
+
+    return new Promise((resolve, reject) => {
+      let url = LinkReplaceParams(content.submit_api || '', this.params);
+      //这里开始上传
+      this.request.post(url, row).subscribe({
+        next: (res) => {
+          if (res.error) {
+            reject(res.error)
+            return
+          }
+          resolve(res.data)
+        },
+        error: (err) => {
+          reject(err)
+        }
+      })
+    })
+  }
+
+  finish() {
+
+  }
+
+  downloadSucceed() {
+    const sheet = utils.json_to_sheet(this.succeed)
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, sheet)
+    const filename = [this.app, this.page, dayjs().format('YYYYMMDDHHmmss'), "succeed.xlsx"].filter(i => i).join("-")
+    writeFile(wb, filename, {compression: true});
+  }
+
+  downloadFailed() {
+    const sheet = utils.json_to_sheet(this.failed)
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, sheet)
+    const filename = [this.app, this.page, dayjs().format('YYYYMMDDHHmmss'), "failed.xlsx"].filter(i => i).join("-")
+    writeFile(wb, filename, {compression: true});
+  }
 }
